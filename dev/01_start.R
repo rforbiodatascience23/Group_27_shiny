e07da36c8b78098765b4a5cb5d9a317104b769a4
Mipiha <mikkelpiester@gmail.com>
2023-10-31 09:52:46 +0100

    5

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,12 @@
 ##
 golem::fill_desc(
   pkg_name = "APPyes", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL, # The URL of the GitHub Repo (optional),
+  pkg_title = "Replicate central dogma", # The Title of the package containing the App
+  pkg_description = "The central dogma of molecular biology", # The Description of the package containing the App
+  author_first_name = "Mikkel", # Your First Name
+  author_last_name = "Hansen", # Your Last Name
+  author_email = "mikkelpiester@gmail.com", # Your Email
+  repo_url = 'https://github.com/rforbiodatascience23/Group_27_shiny/' , # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
 
